e4dcfb72d2684e85b5f66cf4503e63f699ac8994
Abdelrahman Ghazaly <abdelrahman.ghazaly2101@gmail.com>
2022-09-28 23:56:53 +0200

    life_note

--- lib/ui/screens/onboarding_screen/onboarding_screen.dart
+++ lib/ui/screens/onboarding_screen/onboarding_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
-import 'package:life_note/ui/screens/onboarding_screen/intro_screen.dart';
+import 'package:life_note/common/constants/constants.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({
@@ -14,28 +14,10 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
-    List<PageViewModel> pages = [
-      IntroScreen(
-        bigText: 'Life Note',
-        smallText: 'Record Your Life\nNever Miss A Moment',
-        imageUrl: r'assets/images/laptop_girl_1.gif',
-      ),
-      IntroScreen(
-        bigText: 'Dear Dirary',
-        smallText: 'Record Your Life\nNever Miss A Moment',
-        imageUrl: r'assets/images/boy_studying.gif',
-      ),
-      IntroScreen(
-        bigText: 'Note',
-        smallText: 'Record Your Life\nNever Miss A Moment',
-        imageUrl: r'assets/images/laptop_girl_2.gif',
-      ),
-    ];
-
     return SafeArea(
       child: Scaffold(
         body: IntroductionScreen(
-          pages: pages,
+          pages: kIntroductionPages,
           showNextButton: true,
           next: const Icon(
             Icons.arrow_forward_ios,
---
+++ lib/ui/ui.dart
@@ -0,0 +1,3 @@
+export 'screens/onboarding_screen/intro_screen.dart';
+export 'screens/onboarding_screen/onboarding_screen.dart';
+export 'screens/onboarding_screen/splash_screen.dart';
