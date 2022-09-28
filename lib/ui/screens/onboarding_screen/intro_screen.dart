2c779f3aa7be5ad8d7e72d5d68dd0c97062a5594
Abdelrahman Ghazaly <abdelrahman.ghazaly2101@gmail.com>
2022-09-28 23:47:10 +0200

    life_note

--- lib/ui/screens/onboarding_screen/intro_screen.dart
+++ lib/ui/screens/onboarding_screen/intro_screen.dart
@@ -1,52 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:life_note/utilities/utilities.dart';
 
-class IntroScreen extends StatelessWidget {
-  const IntroScreen({
+class IntroScreen extends PageViewModel {
+  IntroScreen({
     Key? key,
     required this.bigText,
     required this.smallText,
     required this.imageUrl,
-  }) : super(key: key);
+  });
   final String bigText;
   final String smallText;
   final String imageUrl;
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  height: Utilities.screenHeight * 0.35,
-                  width: Utilities.screenWidth * 0.7,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(imageUrl),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Text(
-              bigText,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Text(
-              smallText,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
+  PageViewModel build(BuildContext context) {
+    return PageViewModel(
+      title: bigText,
+      body: smallText,
+      image: Container(
+        height: Utilities.screenHeight * 0.35,
+        width: Utilities.screenWidth * 0.7,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(imageUrl),
+          ),
         ),
       ),
     );
--- lib/ui/screens/onboarding_screen/onboarding_screen.dart
+++ lib/ui/screens/onboarding_screen/onboarding_screen.dart
@@ -1,16 +1,11 @@
-import 'package:concentric_transition/page_view.dart';
 import 'package:flutter/material.dart';
-import 'package:life_note/constants/theme/app_colors/app_color.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:life_note/ui/screens/onboarding_screen/intro_screen.dart';
-import 'package:life_note/ui/screens/splash_screen/splash_screen.dart';
-import 'package:life_note/utilities/utilities.dart';
 
 class OnboardingScreen extends StatefulWidget {
-  const OnboardingScreen(
-      {Key? key, required this.bigText, required this.smallText})
-      : super(key: key);
-  final String bigText;
-  final String smallText;
+  const OnboardingScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<OnboardingScreen> createState() => _OnboardingScreenState();
@@ -19,44 +14,38 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
-    List<Widget> pages = [
-      const IntroScreen(
+    List<PageViewModel> pages = [
+      IntroScreen(
         bigText: 'Life Note',
         smallText: 'Record Your Life\nNever Miss A Moment',
         imageUrl: r'assets/images/laptop_girl_1.gif',
       ),
-      const IntroScreen(
+      IntroScreen(
         bigText: 'Dear Dirary',
         smallText: 'Record Your Life\nNever Miss A Moment',
         imageUrl: r'assets/images/boy_studying.gif',
       ),
-      const IntroScreen(
+      IntroScreen(
         bigText: 'Note',
         smallText: 'Record Your Life\nNever Miss A Moment',
         imageUrl: r'assets/images/laptop_girl_2.gif',
       ),
-      const SplashScreen(),
     ];
 
     return SafeArea(
       child: Scaffold(
-        body: ConcentricPageView(
-          radius: 30,
-          verticalPosition: 0.85,
-          colors: const [
-            AppColorDark.buttonColor,
-            AppColorDark.backgroundColor,
-          ],
-          itemCount: 4,
-          itemBuilder: (int index) {
-            if (index == 3) {
-              Utilities().navigatTo(
-                context: context,
-                screen: const SplashScreen(),
-              );
-            }
-            return pages[index];
-          },
+        body: IntroductionScreen(
+          pages: pages,
+          showNextButton: true,
+          next: const Icon(
+            Icons.arrow_forward_ios,
+            size: 20,
+          ),
+          nextStyle: const ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(Colors.transparent),
+          ),
+          showDoneButton: false,
+          isProgress: false,
         ),
       ),
     );
