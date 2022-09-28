5ed2bc51d2ecfdbd8c984ecdde1760f6b2793a36
Abdelrahman Ghazaly <abdelrahman.ghazaly2101@gmail.com>
2022-09-28 23:52:34 +0200

    life_note

--- lib/ui/screens/onboarding_screen/onboarding_screen.dart
+++ lib/ui/screens/onboarding_screen/onboarding_screen.dart
@@ -1,14 +1,6 @@
-import 'package:concentric_transition/page_view.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:introduction_screen/introduction_screen.dart';
-=======
-import 'package:life_note/common/widgets/bottom_bar.dart';
-import 'package:life_note/constants/theme/app_colors/app_color.dart';
->>>>>>> 61c6200d3b468e786fe9f4b7fa3fb1a158bc77d3
 import 'package:life_note/ui/screens/onboarding_screen/intro_screen.dart';
-import 'package:life_note/ui/screens/splash_screen/splash_screen.dart';
-import 'package:life_note/utilities/utilities.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({
@@ -42,7 +34,6 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
     return SafeArea(
       child: Scaffold(
-<<<<<<< HEAD
         body: IntroductionScreen(
           pages: pages,
           showNextButton: true,
@@ -55,22 +46,6 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           ),
           showDoneButton: false,
           isProgress: false,
-=======
-        body: ConcentricPageView(
-          radius: 30,
-          verticalPosition: 0.85,
-          colors: const [
-            AppColorDark.buttonColor,
-            AppColorDark.backgroundColor,
-          ],
-          itemCount: 4,
-          itemBuilder: (int index) => index == 3
-              ? Utilities().navigatTo(
-                  context: context,
-                  screen: const BottomBar(),
-                )
-              : pages[index],
->>>>>>> 61c6200d3b468e786fe9f4b7fa3fb1a158bc77d3
         ),
       ),
     );
