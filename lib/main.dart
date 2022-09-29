4865c38055710724c899de28118c02f2ec6c383c
osama <osama-abdelrassoul>
2022-09-30 01:07:05 +0200

    life_note

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Life Note',
-        theme: getLightThemeData(),
+        theme: getDarkThemeData(),
         home: const OnboardingScreen());
   }
 }
